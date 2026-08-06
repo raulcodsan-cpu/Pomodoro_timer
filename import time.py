c14b0306b1e65dfeaf5493c74baea3987b2abc36
Raul Codina <raulcodsan@gmail.com>
2026-08-06 22:11:13 +0900

    Phase 3 done

--- import time.py
+++ import time.py
@@ -1,14 +1,30 @@
 import time
+import subprocess
 
 WORK_MINUTES = 25
 SHORT_BREAK = 5
 LONG_BREAK = 15
 SESSIONS = 4
 
+def notify(title, message):
+    try: #Error handling
+        subprocess.run([
+            "notify-send",
+            "--urgency=normal",
+            "--icon=clock",
+            title,
+            message
+            ])
+    except FileNotFoundError:
+        print(f"[notify] {title}: {message}") #Fall back to Terminal if notify-send is not available
+
 def countdown(minutes, label):
     total_seconds = minutes*60
+    number = 10
+    
     
     while(total_seconds>0):
+        
         mins = total_seconds // 60
         secs = total_seconds % 60
         display = f"{label} : {mins:02d} : {secs:02d}"
@@ -19,11 +35,14 @@ def countdown(minutes, label):
     
 def run_session(session_num):
     print(f"\n-------- Pomodoro {session_num} of {SESSIONS} -----------")
+    notify("🍅 Work time!", f"Pomodoro {session_num} of {SESSIONS} starting.")
     countdown(WORK_MINUTES, "Work")
     
     if(session_num < SESSIONS):
+        notify("☕ Short break", "5 minutes. Step away!")
         countdown(SHORT_BREAK, "Short Break")
     else:
+        notify("🎉 Long break!", "15 minutes. You earned it.")
         countdown(LONG_BREAK,"Long Break")
 
 def main():
@@ -31,7 +50,7 @@ def main():
     
     for i in range(1, SESSIONS+1):
         run_session(i)
-    
+    notify("✅ All done!", "Great work today.")
     print("All sessions completed!")
 
 main()        
